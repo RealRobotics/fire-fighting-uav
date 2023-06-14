38d2c3d8960fd12cd9925e0a0f8cb91deb04f569
Lenka Mudrich <lenka.mudrich@gmail.com>
2023-06-14 14:45:57 +0100

    Change code to work with the newest msg

--- battery_monitor/src/battery_monitor.cpp
+++ battery_monitor/src/battery_monitor.cpp
@@ -8,20 +8,26 @@
 #include "std_msgs/String.h"
 
 #include "battery_monitor/BatteryStatus.h"
+#include "battery_monitor/InputAccepted.h"
 
 ros::Publisher battery_status_pub;
-ros::Publisher input_pub; //TODO(#1)
+ros::Publisher input_status_pub;
 //the limit under which battery monitor will start to publish safety critical msg, -1 is invalid setting, 100 is max value
 std::atomic<int8_t> safety_threshold {-1};
 //the limit under which battery monitor will start to publish mission critical msg, -1 is invalid setting, 100 is max value
-std::atomic<int8_t> mission_threshold {-1};			
-//max percentage level for the battery, technically dji_sdk should never return number bigger than 1 but it doesnt hurt to make this code safe																
+std::atomic<int8_t> mission_threshold {-1};
+//max percentage level for the battery, technically drones should never return number bigger than 1 but it doesnt hurt to make this code safe
 constexpr int8_t kMaxBatteryLevel {100};
-//min percentage level for the battery, technically dji_sdk should never return number smaller than 0 but it doesnt hurt to make this code safe																
+//min percentage level for the battery, technically drones should never return number smaller than 0 but it doesnt hurt to make this code safe
 constexpr int8_t kMinBatteryLevel {0};
 
 void BatteryCallback(const sensor_msgs::BatteryState::ConstPtr& msg)
-{	
+{
+  battery_monitor::InputAccepted msg2;
+  msg2.data = true;
+  msg2.header = msg->header;
+  input_status_pub.publish(msg2);
+
   battery_monitor::BatteryStatus output_msg;
   output_msg.status = battery_monitor::BatteryStatus::UNSET;
 
@@ -32,21 +38,15 @@ void BatteryCallback(const sensor_msgs::BatteryState::ConstPtr& msg)
       output_msg.status = battery_monitor::BatteryStatus::OK;
     } else if ((current_battery <= mission_threshold) && (current_battery > safety_threshold )) {
       output_msg.status = battery_monitor::BatteryStatus::MISSION_CRITICAL;
-    } else if ((current_battery <= safety_threshold) && (current_battery >= kMinBatteryLevel)) {		
+    } else if ((current_battery <= safety_threshold) && (current_battery >= kMinBatteryLevel)) {
       output_msg.status = battery_monitor::BatteryStatus::SAFETY_CRITICAL;
     } else {
       ROS_ERROR("Invalid battery reading.");
       output_msg.status = battery_monitor::BatteryStatus::BATTERY_READING_OUT_OF_SCOPE;
     }
   }
-
-  //TODO(#1)
-  std_msgs::Bool msg2;
-  msg2.data = true;
-  input_pub.publish(msg2);
-
   battery_status_pub.publish(output_msg);
-}	
+}
 
 int8_t LoadThreshold(ros::NodeHandle nh, std::string threshold_name) {
   int read_threshold {-1};
@@ -56,7 +56,7 @@ int8_t LoadThreshold(ros::NodeHandle nh, std::string threshold_name) {
     } else {
       ROS_ERROR("Value for %s must be between 0 and 100.", threshold_name.c_str());
       ros::shutdown();
-    }   
+    }
   } else {
     ROS_ERROR("Failed to get param %s", threshold_name.c_str());
     ros::shutdown();
@@ -76,11 +76,11 @@ int main(int argc, char **argv)
     ROS_ERROR("Mission threshold is below Safety threshold! Please fix the settings.");
     ros::shutdown();
   }
-  
-  input_pub = nh.advertise<std_msgs::Bool>("input_accepted",1000);
+
+  input_status_pub = nh.advertise<battery_monitor::InputAccepted>("input_accepted",1000);
   battery_status_pub = nh.advertise<battery_monitor::BatteryStatus>("battery_status", 1000);
 
-  ros::Subscriber battery_sub = nh.subscribe("dji_sdk/battery_state", 10, BatteryCallback);
+  ros::Subscriber battery_sub = nh.subscribe("/fcs/battery_state", 10, BatteryCallback);
 
   ROS_INFO("Battery monitor is running");
 
